fbd380acf44d4b3e719752f84dc0d66075e88396
TimurDian <timur7dian84@gmail.com>
2021-02-23 19:51:35 +0700

    Initial Commit

---
+++ app/Http/Controllers/CashController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CashResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class CashController extends Controller
+{
+    public function index()
+    {
+        $debit = Auth::user()->cashes()->whereBetween('when', [now()->firstOfMonth(), now()])->where('amount', '>=', 0)->get('amount')->sum('amount');
+        $credit = Auth::user()->cashes()->whereBetween('when', [now()->firstOfMonth(), now()])->where('amount', '<=', 0)->get('amount')->sum('amount');
+        $transactions = Auth::user()->cashes()->whereBetween('when', [now()->firstOfMonth(), now()])->latest()->get();
+
+        $balances = Auth::user()->cashes()->get('amount')->sum('amount');
+        return response()->json([
+            'debit' => formatPrice($debit),
+            'credit' => formatPrice($credit),
+            'balances' => formatPrice($balances),
+            'transactions' => CashResource::collection($transactions),
+        ]);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'name' => 'required',
+            'amount' => 'required|numeric',
+        ]);
+
+        $slug = request('name') . '-' . Str::random(6);
+        $when = request('when') ?? now();
+        Auth::user()->cashes()->create([
+            'name' => request('name'),
+            'slug' => Str::slug($slug),
+            'when' => $when,
+            'amount' => request('amount'),
+            'description' => request('description'),
+        ]);
+
+        return response()->json([
+            'message' => 'The transaction has been saved',
+        ]);
+    }
+}
